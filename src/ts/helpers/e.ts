0bf2f8537c7d44edbcd4e02ad7f9a8f6870390fb
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2024-05-30 00:50:50 -0400

    Flatten /dist for GitHub Pages

--- src/ts/helpers/e.ts
+++
@@ -1,3 +0,0 @@
-export function E (x: number) {
-  return Math.pow(10, x);
-}
--- src/ts/helpers/pad.ts
+++
@@ -1,4 +0,0 @@
-export function pad(str: any, max: number): String {
-  str = str.toString();
-  return str.length < max ? pad("0" + str, max) : str;
-}
